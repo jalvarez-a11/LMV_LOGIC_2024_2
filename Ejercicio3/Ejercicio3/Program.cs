b2835fb3eaaa577a102717e92beaa765d92fac41
jose manuel alvarez <josealeman93@hotmail.com>
2024-10-22 07:58:19 -0500

    avance ejercicio3

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+       // Un vendedor recibe un sueldo base más un 10 % extra por comisión de sus ventas, el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones.
+       // datos de entrada
+       double salario = 2500000;
+        const double comision = 0.1;
+        int venta1 = 250000;
+        int venta2 = 300000;
+        int venta3 = 450000;
+        // proceso
+
+    }
+}
