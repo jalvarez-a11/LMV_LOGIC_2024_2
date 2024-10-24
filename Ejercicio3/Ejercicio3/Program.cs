f8ff6568db0cd2adc1310abc0d6ecb977c99acad
jose manuel alvarez <josealeman93@hotmail.com>
2024-10-24 10:11:03 -0500

    tercer ejercicio terminado

--- Ejercicio3/Ejercicio3/Program.cs
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -6,12 +6,27 @@ public class Program
     {
        // Un vendedor recibe un sueldo base más un 10 % extra por comisión de sus ventas, el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones.
        // datos de entrada
-       double salario = 2500000;
-        const double comision = 0.1;
-        int venta1 = 250000;
-        int venta2 = 300000;
-        int venta3 = 450000;
+       double Salario = 2500000;
+       double Comision_Venta1, Comision_Venta2, Comision_Venta3, ComisionTotal;
+        Console.Write("ingrese el valor de la primera venta ");
+        double Venta1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor de la segunda venta ");
+        double Venta2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor de la tercera venta ");
+        double Venta3 = Convert.ToDouble(Console.ReadLine());
+
         // proceso
 
+        Comision_Venta1 = Venta1 * 0.10;
+        Comision_Venta2 = Venta2 * 0.10;
+        Comision_Venta3 = Venta3 * 0.10;
+
+        ComisionTotal = Comision_Venta1 + Comision_Venta2 + Comision_Venta3;
+
+        double TotalSueldo = Salario + ComisionTotal;
+        //venta mayor comision
+        double MayorComision = Math.Max(Comision_Venta1, Math.Max(Comision_Venta2, Comision_Venta3));
     }
 }
