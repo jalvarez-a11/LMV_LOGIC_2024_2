fbc439602d464752c72f4e3b3d4958e263c186c9
jose manuel alvarez <josealeman93@hotmail.com>
2024-10-30 17:28:49 -0500

    trabajo terminado

---
+++ Ejercicio32/Ejercicio32/Estudiantes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio32
+{
+    public class Estudiantes
+    {
+        public int Estrato { get; set; }
+        public int Creditos { get; set; }
+        public double ValorCredito { get; set; }
+
+        // metodos de mi clase Estudiantes
+        public double CalcularMatricula()
+        {
+
+            double valorMatricula = 0;
+            if (Creditos <= 20)
+            {
+                valorMatricula = Creditos * ValorCredito;
+            }
+            else
+            {
+                valorMatricula = 20 * ValorCredito + (Creditos - 20) * ValorCredito
+     * 2;
+            }
+
+            // Aplicar descuento por estrato
+            switch (Estrato)
+            {
+                case 1:
+                    valorMatricula *= 0.2; // Descuento del 80%
+                    break;
+                case 2:
+                    valorMatricula *= 0.5; // Descuento del 50%
+                    break;
+                case 3:
+                    valorMatricula *= 0.7; // Descuento del 30%
+                    break;
+            }
+
+            return valorMatricula;
+        }
+
+        public double CalcularSubsidio()
+        {
+            switch (Estrato)
+            {
+                case 1:
+                    return 200000;
+                case 2:
+                    return 100000;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- Ejercicio32/Ejercicio32/Program.cs
+++ Ejercicio32/Ejercicio32/Program.cs
@@ -1,8 +1,33 @@
-﻿public class Program
+﻿using Ejercicio32;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-       // datos de entrada: numero de creditos, estracto socioeconomico, subcidio de alimentacion y transporte
+        // datos de entrada: numero de creditos, estracto socioeconomico, subcidio de alimentacion y transporte, valor por credito
 
+        bool continuar = true;
+        while (continuar)
+        {
+            Estudiantes estudiante = new Estudiantes();
+
+            Console.Write("Ingrese el estrato: ");
+            estudiante.Estrato = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de créditos: ");
+            estudiante.Creditos = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor por crédito: ");
+            estudiante.ValorCredito = double.Parse(Console.ReadLine());
+
+            double matricula = estudiante.CalcularMatricula();
+            double subsidio = estudiante.CalcularSubsidio();
+
+            Console.WriteLine($"Costo de la matrícula: ${matricula}");
+            Console.WriteLine($"Subsidio: ${subsidio}");
+
+            Console.Write("¿Desea calcular otra matrícula? (s/n): ");
+            continuar = Console.ReadLine().ToLower() == "s";
+        }
     }
 }
